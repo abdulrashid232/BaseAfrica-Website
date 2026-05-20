a1ec1d05a49688da0124e2fd782549750ee81208
Magnus <abeikumagnus@gmail.com>
2026-05-20 21:15:32 +0100

    refactor(app): move main layout components to home feature module

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './core/header/header';
-import { HeroSection } from './core/hero-section/hero-section';
-import { WhyChooseUs } from './core/why-choose-us/why-choose-us';
-import { Feedback } from './core/feedback/feedback';
-import { Operations } from './core/operations/operations';
-import { Footer } from './core/footer/footer';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, HeroSection, Operations, WhyChooseUs, Feedback, Footer  ],
+  imports: [RouterOutlet ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Operations } from "../../core/operations/operations";
+import { Feedback } from "../../core/feedback/feedback";
+import { Footer } from "../../core/footer/footer";
+import { WhyChooseUs } from "../../core/why-choose-us/why-choose-us";
+import { HeroSection } from "../../core/hero-section/hero-section";
+import { Header } from "../../core/header/header";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Operations, Feedback, Footer, WhyChooseUs, HeroSection, Header, RouterOutlet],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
